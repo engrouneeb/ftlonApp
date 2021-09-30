90a83337191ff90521fe77d4f6fdfdbdbcf5e615
engrouneeb <37792643+engrouneeb@users.noreply.github.com>
2021-10-01 03:26:10 +0500

    Order pages changes Done

--- src/screen/Order/index.js
+++ src/screen/Order/index.js
@@ -1,11 +1,125 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-
+import { StyleSheet, Text, View,SafeAreaView,Image,FlatList, Pressable } from 'react-native'
+import{Icon, ScrollView} from "native-base";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { Colors, Typography } from "../../styles";
+import{Card} from "react-native-paper";
+import ScreensNames from '../../navigations/stack/screenName';
+const DATA = [
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/Toor-Dal_website_front_900x.jpg?v=1626191872"
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/Toor-Dal_website_back_1024x1024@2x.jpg?v=1626191872"
+      },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/Toor-Dal_website_front_900x.jpg?v=1626191872"
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/Toor-Dal_website_back_1024x1024@2x.jpg?v=1626191872"
+    },
+  ];
 const Orders = () => {
     return (
-        <View style={styles.center}> 
-            <Text>Orders</Text>
-        </View>
+        <SafeAreaView style={{backgroundColor:Colors.WHITE,height:"100%"}}>
+         <View style={{flexDirection:"row",borderBottomWidth:.25,width:"92%",borderColor:Colors.GRAY,alignSelf:"center",marginTop:25,paddingBottom:10}}>
+            <Text style={{fontWeight:Typography.FONT_WEIGHT_BOLD,color:Colors.TEXT_COLOR+60,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%"}}>3 Orders</Text>
+         </View>
+         <ScrollView>
+         <>    
+         <View>
+         <View style={{flexDirection:"row"}}>
+         <View style={{marginLeft:20,marginTop:20,borderBottomColor:Colors.PRIMARY,borderBottomWidth:3,height:40,width:"27%"}}>
+             <Text style={{fontWeight:Typography.FONT_WEIGHT_BOLD,color:Colors.BLACK,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%",textTransform:"uppercase",}}>Processing</Text>
+         </View>
+         <View style={{marginLeft:0,marginTop:20,borderBottomColor:Colors.BLACK+20,borderBottomWidth:3,height:40,width:"64%"}}>
+        <Pressable onPress={()=>{}}>
+         <Icon as={AntDesign} name="right" color={Colors.PRIMARY} size={5} style={{position:"absolute",right:10,top:5,fontWeight:"bold"}}/>
+         </Pressable>
+         </View>
+         </View>
+         <View style={{flexDirection:"row",width:"100%",height:100,marginLeft:20,}}>
+             {DATA.map((item)=>{
+                 return(
+                            <Image 
+                                source={{uri:item.url}} 
+                                style={{width:"20%",borderBottomColor:Colors.TEXT_COLOR+50,
+                                marginHorizontal:5,
+                                borderBottomWidth:1,}} resizeMode={"contain"} >
+                            </Image>
+                 )
+             })}
+         </View>
+         <View style={{borderBottomWidth:.25,width:"90%",borderColor:Colors.GRAY,marginLeft:20}}>
+         <Text style={{fontWeight:"300",color:Colors.TEXT_COLOR,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%",marginTop:20}}>Order №: 38473737</Text>
+         <Text style={{fontWeight:"300",color:Colors.TEXT_COLOR,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%",marginTop:5}}>Order date: 5 Jul 2021</Text>
+         </View>
+         </View>
+         <View>
+         <View>    
+         <View style={{flexDirection:"row"}}>
+         <View style={{marginLeft:20,marginTop:20,borderBottomColor:Colors.PRIMARY,borderBottomWidth:3,height:40,width:"60%"}}>
+             <Text style={{fontWeight:Typography.FONT_WEIGHT_BOLD,color:Colors.BLACK,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%",textTransform:"uppercase",}}>SENT</Text>
+         </View>
+         <View style={{marginLeft:0,marginTop:20,borderBottomColor:Colors.BLACK+20,borderBottomWidth:3,height:40,width:"30%"}}>
+        <Pressable onPress={()=>{}}>
+         <Icon as={AntDesign} name="right" color={Colors.PRIMARY} size={5} style={{position:"absolute",right:10,top:5,fontWeight:"bold"}}/>
+         </Pressable>
+         </View>
+         </View>
+         <View style={{flexDirection:"row",width:"100%",height:100,marginLeft:20,}}>
+             {DATA.map((item)=>{
+                 return(
+                            <Image 
+                                source={{uri:item.url}} 
+                                style={{width:"20%",borderBottomColor:Colors.TEXT_COLOR+50,
+                                marginHorizontal:5,
+                                borderBottomWidth:1,}} resizeMode={"contain"} >
+                            </Image>
+                 )
+             })}
+         </View>
+         </View>
+         <View style={{flexDirection:"row",borderBottomWidth:.25,width:"90%",borderColor:Colors.GRAY,alignSelf:"center",marginTop:25,paddingBottom:10,}}>
+            <Text style={{fontWeight:"300",color:Colors.PRIMARY,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%"}}>Track Order</Text>
+         </View>
+         <View style={{borderBottomWidth:.25,width:"90%",borderColor:Colors.GRAY,marginLeft:20}}>
+         <Text style={{fontWeight:"300",color:Colors.TEXT_COLOR,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%",marginTop:20}}>Order №: 38473737</Text>
+         <Text style={{fontWeight:"300",color:Colors.TEXT_COLOR,fontSize:Typography.FONT_SIZE_14,marginBottom:15,width:"85%",marginTop:5}}>Order date: 5 Jul 2021</Text>
+         </View>
+         </View>
+         <View>
+         <View style={{flexDirection:"row"}}>
+         <View style={{marginLeft:20,marginTop:20,borderBottomColor:Colors.PRIMARY,borderBottomWidth:3,height:40,width:"27%"}}>
+             <Text style={{fontWeight:Typography.FONT_WEIGHT_BOLD,color:Colors.BLACK,fontSize:Typography.FONT_SIZE_14,marginBottom:5,width:"85%",textTransform:"uppercase",}}>DELIVERED</Text>
+         </View>
+         <View style={{marginLeft:0,marginTop:20,borderBottomColor:Colors.PRIMARY,borderBottomWidth:3,height:40,width:"62%"}}>
+        <Pressable onPress={()=>{}}>
+         <Icon as={AntDesign} name="right" color={Colors.PRIMARY} size={5} style={{position:"absolute",right:10,top:5,fontWeight:"bold"}}/>
+         </Pressable>
+         </View>
+         </View>
+         <View style={{flexDirection:"row",width:"100%",height:100,marginLeft:20,}}>
+             {DATA.map((item)=>{
+                 return(
+                            <Image 
+                                source={{uri:item.url}} 
+                                style={{width:"20%",borderBottomColor:Colors.TEXT_COLOR+50,
+                                marginHorizontal:5,
+                                borderBottomWidth:1,}} resizeMode={"contain"} >
+                            </Image>
+                 )
+             })}
+         </View>
+         </View>
+         </>
+         </ScrollView>
+         </SafeAreaView>
     )
 }
 const styles = StyleSheet.create({
--- src/screen/Shop/index.js
+++ src/screen/Shop/index.js
@@ -6,26 +6,70 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import { Colors, Typography } from "../../styles";
 import{Card} from "react-native-paper";
 import ScreensNames from '../../navigations/stack/screenName';
-const DATA = [
+const gethr = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
     title: 'Pulses',
-    url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/pinksaltpowder_1024x1024@2x.jpg?v=1625479863"
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Grains',
-    url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/groundnut_oil_2_1024x1024@2x.jpg?v=1624807083"
+    url:require('../../assets/images/dal.png')
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
     title: 'Oils',
-    url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/Gandhasalu_website_3_1024x1024@2x.jpg?v=1628842154"
+    url:require('../../assets/images/oil.png')
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Grains',
+    url:require('../../assets/images/grains.png')
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
     title: 'Salts',
-    url:"https://cdn.shopify.com/s/files/1/0473/0440/9250/products/Almond_900x.jpg?v=1625479760"
+    url:require('../../assets/images/Salt.png')
+  },
+];
+const nurtr = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Plants',
+    url:require('../../assets/images/plants2.png')
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Pots',
+    url:require('../../assets/images/empty_pot.png')
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Garden tools',
+    url:require('../../assets/images/gardentool.png')
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Lanterns',
+    url:require('../../assets/images/lat.png')
+  },
+];
+const elixr = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Soap',
+    url:require('../../assets/images/soap1.png')
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Shampoo',
+    url:require('../../assets/images/shampo1.png')
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Cream',
+    url:require('../../assets/images/Cream.png')
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Hair products',
+    url:require('../../assets/images/brown_hair.png')
   },
 ];
 
@@ -37,8 +81,8 @@ const Item = ( {item}) => (
   height:65}}>
     <View style={{flexDirection:"row",marginTop:10}}>
                    <Image 
-                      source={{uri:item.url}} 
-                      style={{height:50,width:50,marginHorizontal:10,borderRadius:5}}  >
+                      source={item.url} 
+                      style={{height:40,width:40,marginHorizontal:10}}  >
                     </Image>
     <Text style={{alignSelf:"center"}}>{item.title}</Text>
     </View>
@@ -50,64 +94,59 @@ const Item = ( {item}) => (
     <Item item={item} />
   );
     return (
-        <SafeAreaView style={{backgroundColor:Colors.WHITE}}>
+        <SafeAreaView style={{backgroundColor:Colors.WHITE,height:"100%"}}>
            <View style={{flexDirection:"row",borderBottomWidth:.25,width:"92%",borderColor:Colors.GRAY,marginBottom:7,alignSelf:"center",marginTop:25,paddingBottom:10}}>
               <Text style={{fontWeight:Typography.FONT_WEIGHT_BOLD,color:Colors.GRAY,fontSize:Typography.FONT_SIZE_16,marginBottom:5,width:"85%"}}>Search Products</Text>
               <Icon as={AntDesign} name="search1" color={Colors.BLACK} size={5} style={{marginRight:10}}/>
               <Icon as={MaterialCommunityIcons} name="qrcode-scan" color={Colors.BLACK} size={4}/>
             </View>
-            <View style={{flexDirection:"row",marginLeft:-20}}>             
+            <View style={{flexDirection:"row",width:"90%",}}>             
             <Image 
                 source={require("../../assets/images/gathr.png")} 
-                style={{ marginTop:22,width:"50%",height:35}} resizeMode={"contain"} >
+                style={{ marginTop:5,width:"45%",height:35}} resizeMode={"contain"} >
            </Image>
-           <View style={{flex:.4,justifyContent:"center",alignItems:"center",flexDirection:"row",marginTop:22}}>
-           <Pressable style={{flexDirection:"row"}} onPress={()=>{props.navigation.navigate(ScreensNames.Gathr.name)}}>
-           <Text style={{fontSize:12,fontWeight:"400",color:Colors.TEXT_COLOR,marginLeft:60}}>View all</Text>
+           <Pressable style={{flexDirection:"row",alignSelf:"center",marginLeft:30,marginTop:15}} onPress={()=>{props.navigation.navigate(ScreensNames.Gathr.name)}}>
+           <Text style={{fontSize:12,fontWeight:"400",color:Colors.TEXT_COLOR,marginLeft:60,}}>View all</Text>
            <Icon as={AntDesign} name="arrowright" color={Colors.TEXT_COLOR} size={4} style={{marginLeft:10}}/>
            </Pressable>
            </View>
-           </View>
            <FlatList
-              data={DATA}
+              data={gethr}
               numColumns={2}
               style={{height:160}}
               renderItem={renderItem}
               keyExtractor={item => item.id}
             />
-           <View style={{flexDirection:"row",paddingLeft:20}}>           
+           <View style={{flexDirection:"row",width:"90%",}}>           
             <Image 
                 source={require("../../assets/images/nurtr_logo.png")} 
-                style={{ marginTop:22,width:"50%",height:40}} resizeMode={"contain"} >
+                style={{ marginTop:5,width:"45%",height:40}} resizeMode={"contain"} >
            </Image>
-           <View style={{flex:.4,justifyContent:"center",alignItems:"center",flexDirection:"row",marginTop:22}}>
-           <Pressable style={{flexDirection:"row"}} onPress={()=>{props.navigation.navigate(ScreensNames.Nurtr.name)}}>
-           <Text style={{fontSize:12,fontWeight:"400",color:Colors.TEXT_COLOR,marginLeft:30}}>View all</Text>
+           
+           <Pressable style={{flexDirection:"row",alignSelf:"center",marginLeft:40,marginTop:15}} onPress={()=>{props.navigation.navigate(ScreensNames.Nurtr.name)}}>
+           <Text style={{fontSize:12,fontWeight:"400",color:Colors.TEXT_COLOR,marginLeft:45}}>View all</Text>
            <Icon as={AntDesign} name="arrowright" color={Colors.TEXT_COLOR} size={4} style={{marginLeft:10}}/>
            </Pressable>
            </View>
-           </View>
            <FlatList
-              data={DATA}
+              data={nurtr}
               numColumns={2}
               style={{height:160}}
               renderItem={renderItem}
               keyExtractor={item => item.id}
             />
-           <View style={{flexDirection:"row",}}>
+           <View style={{flexDirection:"row",width:"90%",}}>
             <Image 
                 source={require("../../assets/images/elixr.png")} 
-                style={{ marginTop:22,width:"50%",height:40}} resizeMode={"contain"} >
+                style={{ marginTop:5,width:"45%",height:40}} resizeMode={"contain"} >
            </Image>
-           <View style={{flex:.4,justifyContent:"center",alignItems:"center",flexDirection:"row",marginTop:20}}>
-             <Pressable style={{flexDirection:"row"}} onPress={()=>{props.navigation.navigate(ScreensNames.Elixr.name)}}>
-              <Text style={{fontSize:12,fontWeight:"400",color:Colors.TEXT_COLOR,marginLeft:30}}>View all</Text>
+             <Pressable style={{flexDirection:"row",alignSelf:"center",marginLeft:40,marginTop:15}} onPress={()=>{props.navigation.navigate(ScreensNames.Elixr.name)}}>
+              <Text style={{fontSize:12,fontWeight:"400",color:Colors.TEXT_COLOR,marginLeft:45}}>View all</Text>
               <Icon as={AntDesign} name="arrowright" color={Colors.TEXT_COLOR} size={4} style={{marginLeft:10}}/>
             </Pressable>
            </View>
-           </View>
            <FlatList
-              data={DATA}
+              data={elixr}
               numColumns={2}
               style={{height:160}}
               renderItem={renderItem}
